d07f8e2822f2a745aef891c1b6cb4f006a41bada
Gibbskuy <roslinaelly65445@gmail.com>
2024-07-31 16:53:23 +0700

    belajar API login logout

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\BeritaController;
 use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user/profile', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
@@ -17,7 +18,15 @@ Route::get('/user', function (Request $request) {
 // Route::put('kategori/{id}', [KategoriController::class, 'update']);
 // Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
 
-Route::resource('kategori', KategoriController::class)->except(['edit', 'create']);
-Route::resource('tag', TagController::class)->except(['edit', 'create']);
-Route::resource('user', UserController::class)->except(['edit', 'create']);
-Route::resource('berita', BeritaController::class)->except(['edit', 'create']);
+// auth route
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::resource('kategori', KategoriController::class)->except(['edit', 'create']);
+    Route::resource('tag', TagController::class)->except(['edit', 'create']);
+    Route::resource('user', UserController::class)->except(['edit', 'create']);
+    Route::resource('berita', BeritaController::class)->except(['edit', 'create']);
+});
+
